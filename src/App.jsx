e7a4c69936232844f4d67cdfdf084c60481549b7
Christopher Wiles <chriswiles87@gmail.com>
2016-10-31 12:48:06 -0500

    create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <MuiThemeProvider>
+    <Provider store={store}>
+
+    </Provider>
+  </MuiThemeProvider>
+)
