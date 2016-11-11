564ed38ceb6221aecbc7055ba672a9016bc1c7b9
Christopher Wiles <chriswiles87@gmail.com>
2016-11-11 12:57:41 -0600

    add theme and routes

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import theme from './material_ui_raw_theme_file'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 // import reducer from './reducers
@@ -13,17 +14,15 @@ import routes from './routes'
 import './index.css'
 
 injectTapEventPlugin()
-
-const store = createStore(e => e, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(e => e, reduxDevtools)
 
 const Root = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={theme}>
     <Provider store={store}>
       <Router history={browserHistory} routes={routes} />
     </Provider>
   </MuiThemeProvider>
 )
 
-render(
-  <Root/>, document.getElementById('root')
-)
+render(<Root/>, document.getElementById('root'))
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,15 +1,14 @@
+import React from 'react'
+import {Route} from 'react-router'
+
 import App from './containers/App'
-import Home from './components/Home.jsx'
 
-const routes = {
-  // base component (wrapper for the whole application).
-  component: App,
-  childRoutes: [
-    {
-      path: '/',
-      component: Home
-    }
-  ]
-}
-
-export default routes
+export default (
+  <Route path="/" component={App}>
+    {/* Show the dashboard at / */}
+    {/* <IndexRoute component={Home}/>
+    <Route path="projects" component={Projects}/>
+    <Route path="about" component={About}/>
+    <Route path="info" component={Info}/> */}
+  </Route>
+)
