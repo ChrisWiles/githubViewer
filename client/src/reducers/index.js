04bf8a47850345ce36ea88775ddfc3ae8456a1ca
Christopher Wiles <chriswiles87@gmail.com>
2016-11-15 13:44:38 -0600

    create 1st reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from 'redux'
-import {client} from '../index'
-
+import {REPO_NAME, REPO_OWNER} from '../constants/actionTypes'
 
 /*
   Things you should never do inside a reducer
@@ -12,6 +11,21 @@ import {client} from '../index'
 // Combines multiple reducers into a single reducing function with each reducer as a
 // key/value pair. Can then be passed to createStore().
 
-const rootReducer = combineReducers({apollo: client.reducer()})
+function repos(state = {repos: [], totalCount: 0}, action) {
+
+  const repoNames = repos => repos.map(repo => repo.node.name)
+
+  switch (action.type) {
+    case REPO_NAME:
+    let {edges, totalCount} = action.payload.data.repositoryOwner.repositories
+      return {repos: repoNames(edges), totalCount}
+    case REPO_OWNER:
+      return action.payload.data
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({repos})
 
 export default rootReducer
