7ee597e8a10b2107a57253d5265ce86ab3c26c47
Christopher Wiles <chriswiles87@gmail.com>
2016-11-15 13:42:55 -0600

    not using react-apollo

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,68 +1,10 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+
 import NavBar from '../components/NavBar'
 import SlideDrawer from '../components/SlideDrawer'
-import {graphql} from 'react-apollo'
+import SmartSearchLogin from './SmartSearchBar'
 
-import gql from 'graphql-tag'
-
-const GET_REPO_INFO = gql `
-  query GetRepoInfo($name: String!, $login: String!) {
-    repositoryOwner(login: $login) {
-      repository(name: $name) {
-        stargazers {
-          totalCount
-        }
-        watchers {
-          totalCount
-        }
-        createdAt
-        description
-        url
-        commitComments(first: 10) {
-          edges {
-            node {
-              author {
-                avatarURL
-                name
-              }
-              body
-              commit {
-                message
-  							committer {
-                  date
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-const withInfo = graphql(GET_REPO_INFO, {
-  options: ({login, name}) => {
-    return {
-      variables: {
-        login: 'facebook',
-        name: 'react'
-      }
-    }
-  },
-  props: ({ownProps, data}) => {
-    // loading state
-    if (data.loading) {
-      return {loading: true}
-    }
-
-    // error state
-    if (data.error) {
-      return { hasErrors: true }
-    }
-    return {data}
-  }
-})
 
 class App extends Component {
   constructor(props) {
@@ -73,9 +15,7 @@ class App extends Component {
     }
   }
 
-  componentWillReceiveProps(props) {
-    console.log(props.data.repositoryOwner.repository)
-  }
+  componentWillReceiveProps(props) {}
 
   handleToggle = () => this.setState({open: !this.state.open})
 
@@ -85,7 +25,8 @@ class App extends Component {
     const {title, open} = this.state
     return (
       <div>
-        <NavBar handleToggle={this.handleToggle} title={title}/>
+        {/* <NavBar handleToggle={this.handleToggle} title={title}/> */}
+        <SmartSearchLogin/>
         <SlideDrawer handleToggle={this.handleToggle} open={open} setTitle={this.setTitle}/>
         {/* child component will be rendered here */}
         {this.props.children}
@@ -95,10 +36,11 @@ class App extends Component {
   }
 }
 
-const AppWithInfo = withInfo(App)
+//const AppWithInfo = withInfo(App)
 const mapStateToProps = (state) => {
   console.log(state)
   return state
 }
 
-export default connect(mapStateToProps)(AppWithInfo)
+//export default connect(mapStateToProps)(AppWithInfo)
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,77 +1,30 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
-import {browserHistory, Router} from 'react-router'
-import {createStore, combineReducers} from 'redux'
-import {ApolloProvider} from 'react-apollo'
-
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import theme from './material_ui_raw_theme_file'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-
-import routes from './routes'
-
-
-// Apollo
-import ApolloClient, {createNetworkInterface} from 'apollo-client'
-
-// Auth
-import {login} from './githubLogin'
-import {username, password} from './config'
-
 import './index.css'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import React from 'react'
+import ReduxPromise from 'redux-promise'
+import rootReducer from './reducers'
+import routes from './routes'
+import theme from './material_ui_raw_theme_file'
+import {browserHistory, Router} from 'react-router'
+import {createStore, applyMiddleware } from 'redux'
+import {Provider} from 'react-redux'
+import {render} from 'react-dom'
 
-// Global.Auth
-let TOKEN = null
-
-
-// Global.Apollo
-const URI = {uri: 'https://api.github.com/graphql'}
-const networkInterface = createNetworkInterface(URI)
-
-networkInterface.use([
-  {
-    applyMiddleware(req, next) {
-      if (!req.options.headers) {
-        req.options.headers = {} // Create the header object if needed.
-      }
-
-      // Send the login token in the Authorization header
-      req.options.headers.authorization = `Bearer ${TOKEN}`
-      next()
-    }
-  }
-])
-
-const client = new ApolloClient({networkInterface})
-
-const rootReducer = combineReducers({apollo: client.reducer()})
 
 injectTapEventPlugin()
+
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(rootReducer, reduxDevtools)
 
-class Root extends Component {
+const middleware = applyMiddleware(ReduxPromise)(createStore)
+const store = middleware(rootReducer, reduxDevtools)
 
-  state = {login: false}
-
-  componentDidMount() {
-    login(username, password).then(token => {
-      console.log('GitHub Token:', token)
-      TOKEN = token
-      this.setState({login: true})
-    })
-  }
-
-  render() {
-    return this.state.login ? (
-      <MuiThemeProvider muiTheme={theme}>
-        <ApolloProvider store={store} client={client}>
-          <Router history={browserHistory} routes={routes} />
-        </ApolloProvider>
-      </MuiThemeProvider>
-    ) : <h1>Logging in...</h1>
-  }
-}
-
+const Root = () => (
+  <Provider store={store}>
+    <MuiThemeProvider muiTheme={theme}>
+      <Router history={browserHistory} routes={routes}/>
+    </MuiThemeProvider>
+  </Provider>
+)
 
 render(<Root/>, document.getElementById('root'))
