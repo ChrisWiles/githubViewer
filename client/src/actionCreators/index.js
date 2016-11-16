d38d10499eb016ce9a61697cfd9a6e069cefa378
Christopher Wiles <chriswiles87@gmail.com>
2016-11-16 08:59:35 -0600

    incorporate redux thunk

--- client/src/actionCreators/index.js
+++ client/src/actionCreators/index.js
@@ -1,15 +1,43 @@
-import {REPO_NAME, REPO_OWNER, RESET_SEARCH} from '../constants/actionTypes'
+/*
+  Async Actions
+  An action informing the reducers that the request began
+  An action informing the reducers that the request finished successfully
+  An action informing the reducers that the request failed
+*/
+
+import {
+  REPO_NAME_REQUEST,
+  REPO_NAME_SUCCESS,
+  REPO_NAME_FAILURE,
+  REPO_OWNER_REQUEST,
+  REPO_OWNER_SUCCESS,
+  REPO_OWNER_FAILURE,
+  RESET_SEARCH,
+  RESET_ERROR_MESSAGE
+} from '../constants/actionTypes'
+
 import {getReposByLogin, getRepoInfoByName} from '../queries'
 import {client} from '../createApolloClient'
 
-export const requestReposNames = (login) => ({
-  type: REPO_NAME,
-  payload: client.query(getReposByLogin({login}))
-})
+export const requestReposNames = (login) => {
+  return dispatch => {
+    dispatch({type: REPO_NAME_REQUEST})
+    return client.query(getReposByLogin({login}))
+      .then(data => dispatch({type: REPO_NAME_SUCCESS, payload: data}))
+      .catch(err => dispatch({type: REPO_NAME_FAILURE, err})
+    )
+  }
+}
 
-export const requestRepoInfo = ({login, name}) => ({
-  type: REPO_OWNER,
-  payload: client.query(getRepoInfoByName({login, name}))
-})
+export const requestRepoInfo = ({login, name}) => {
+  return dispatch => {
+    dispatch({type: REPO_OWNER_REQUEST})
+    return client.query(getRepoInfoByName({login, name}))
+      .then(data => dispatch({type: REPO_OWNER_SUCCESS, payload: data}))
+      .catch(err => dispatch({type: REPO_OWNER_FAILURE, err})
+    )
+  }
+}
 
 export const resetSearch = () => ({type: RESET_SEARCH})
+export const resetErrorMessage = () => ({type: RESET_ERROR_MESSAGE})
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import './index.css'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import React from 'react'
-import ReduxPromise from 'redux-promise'
 import rootReducer from './reducers'
 import routes from './routes'
 import theme from './material_ui_raw_theme_file'
+import thunkMiddleware from 'redux-thunk'
 import {browserHistory, Router} from 'react-router'
 import {createStore, applyMiddleware } from 'redux'
 import {Provider} from 'react-redux'
@@ -16,8 +16,13 @@ injectTapEventPlugin()
 
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const middleware = applyMiddleware(ReduxPromise)(createStore)
-const store = middleware(rootReducer, reduxDevtools)
+const store = createStore(
+  rootReducer,
+  reduxDevtools,
+  applyMiddleware(
+    thunkMiddleware
+  )
+)
 
 const Root = () => (
   <Provider store={store}>
