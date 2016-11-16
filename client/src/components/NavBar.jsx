adb5b08ef8415d9e4b44a8499e82bdd6765e338b
Christopher Wiles <chriswiles87@gmail.com>
2016-11-16 12:54:37 -0600

    user ToolBar over AppBar,  add search and login as child

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,30 @@
 import React, {PropTypes} from 'react'
-import AppBar from 'material-ui/AppBar'
+import RaisedButton from 'material-ui/RaisedButton'
+import IconButton from 'material-ui/IconButton'
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar'
+import Menu from 'material-ui/svg-icons/navigation/menu'
+import SmartSearchLogin from '../containers/SmartSearchBar'
 
-const NavBar = ({title, handleToggle}) => (
-  <AppBar
-      title={title}
-      style={{position:'fixed', background: 'dodgerblue'}}
-      zDepth={1}
-      onLeftIconButtonTouchTap={handleToggle}
-    />
+const label = (bool) => bool ? 'Logout' : "Login"
+
+const NavBar = ({title, handleToggle, isLogin, toggleLogin}) => (
+  <Toolbar>
+    <ToolbarGroup firstChild={true}>
+      <IconButton onTouchTap={handleToggle}><Menu/></IconButton>
+      <ToolbarTitle text={title}/>
+    </ToolbarGroup>
+    <SmartSearchLogin/>
+    <ToolbarGroup lastChild={true}>
+      <RaisedButton primary={true} label={label(isLogin)} onTouchTap={toggleLogin}/>
+    </ToolbarGroup>
+  </Toolbar>
 )
 
 NavBar.propTypes = {
   title: PropTypes.string.isRequired,
-  handleToggle: PropTypes.func.isRequired
+  handleToggle: PropTypes.func.isRequired,
+  isLogin: PropTypes.bool.isRequired,
+  toggleLogin: PropTypes.func.isRequired
 }
 
 export default NavBar
