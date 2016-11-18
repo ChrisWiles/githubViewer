6d43ce22ae6eb954c813875d3c8be8277e3d91ea
Christopher Wiles <chriswiles87@gmail.com>
2016-11-18 12:17:59 -0600

    center snackbar text

--- client/src/components/ContactSnackbar.jsx
+++ client/src/components/ContactSnackbar.jsx
@@ -1,8 +1,14 @@
 import React, {PropTypes} from 'react'
 import Snackbar from 'material-ui/Snackbar'
 
+const bodyStyle = {
+  display: 'flex',
+  justifyContent: 'center'
+}
+
 const ContactSnackbar = ({isOpen, toggle}) => (
   <Snackbar
+    bodyStyle={bodyStyle}
     open={isOpen}
     message="Created by Christopher Wiles"
     autoHideDuration={4000}
