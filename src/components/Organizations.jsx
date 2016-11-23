82ed13d5ea8513e8ed7b327a48a1434b9a36e960
Christopher Wiles <chriswiles87@gmail.com>
2016-11-23 10:05:09 -0600

    create Orginazions comp

---
+++ src/components/Organizations.jsx
@@ -0,0 +1,33 @@
+import React, {PropTypes} from 'react'
+import Avatar from 'material-ui/Avatar'
+import IconButton from 'material-ui/IconButton'
+
+const style = {
+  avatar: {
+    borderRadius: 'none'
+  },
+  root: {
+    marginRight: 10,
+    display: 'block'
+  }
+}
+
+const Organizations = ({organizations}) => (
+  <div style={style.root}>
+    {
+      organizations.map((org, i) => {
+        const {name, avatarURL} = org
+        return (
+          <IconButton tooltip={name} key={i}>
+            <Avatar src={avatarURL} style={style.avatar} backgroundColor='none'/>
+          </IconButton>
+        )
+      })
+     }
+  </div>
+)
+
+Organizations.propTypes = {
+  organizations: PropTypes.array.isRequired
+}
+export default Organizations
